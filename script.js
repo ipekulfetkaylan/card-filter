ff18e067d1b965ce61b54c6106c65e09070b5d32
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-02-13 10:43:22 +0300

    upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const search= ()=>{
+
+    const searchBox= document.getElementById('search-item').value.toUpperCase();
+    const storeItems= document.getElementById('product-list');
+    const product= document.querySelectorAll('.product');
+    const productName= storeItems.getElementsByTagName('h2');
+
+    for(let i= 0; i < productName.length; i++){
+        let match= product[i].getElementsByTagName('h2')[0];
+
+        if(match){
+            let textValue= match.innerHTML || match.textContent;
+            if(textValue.toUpperCase().indexOf(searchBox) > -1){
+                product[i].style.display= '';
+            }else{
+                product[i].style.display= 'none'
+            }
+        }
+    }
+}
